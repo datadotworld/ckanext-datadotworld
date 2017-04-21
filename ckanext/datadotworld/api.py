70f3011ab45aab3de982e5e0650bf0102b6a4644
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2017-04-21 12:44:35 +0300

    DWRLDXCEEZ-2 / alts

--- ckanext/datadotworld/api.py
+++ ckanext/datadotworld/api.py
@@ -25,16 +25,29 @@ licenses = {
 
 
 def notify(pkg_dict, operation):
-    entity = model.Package.get(pkg_dict['id'])
-    orgs = entity.get_groups('organization')
-    if not orgs:
+    owner_org = pkg_dict.get('owner_org')
+    org = model.Group.get(owner_org)
+    if org is None:
         return
-    org = orgs[0]
     credentials = org.datadotworld_credentials
     if credentials is None or not credentials.integration:
         return
+
     api = API(credentials.owner, credentials.key)
-    api.sync(entity)
+    api.sync(pkg_dict)
+
+
+def _prepare_resources(resources):
+    """Convert list of resources to files_list for data.world.
+    """
+    files = []
+    for res in resources:
+        file = os.path.basename(res['url'])
+        files.append(dict(
+            name=file,
+            source=dict(url=res['url'])
+        ))
+    return files
 
 
 class API:
@@ -54,11 +67,12 @@ class API:
         self.owner = owner
         self.key = key
 
-    def sync(self, entity):
+    def sync(self, pkg_dict):
+        entity = model.Package.get(pkg_dict['id'])
         if entity.datadotworld_extras:
-            self._update(entity)
+            self._update(pkg_dict, entity)
         else:
-            self._create(entity)
+            self._create(pkg_dict, entity)
 
     @classmethod
     def generate_link(cls, owner, package=None):
@@ -74,40 +88,29 @@ class API:
             return
         return org.datadotworld_credentials
 
-    def _prepare_resources(self, resources):
-        files = []
-        for res_obj in resources:
-            res = get_action('resource_show')(None, {'id': res_obj.id})
-
-            file = os.path.basename(res['url'])
-            files.append(dict(
-                name=file,
-                source=dict(url=res['url'])
-            ))
-        return files
-
     def _default_headers(self):
         return {
             'Authorization': self.auth.format(key=self.key),
             'Content-type': 'application/json'
         }
 
-    def _create(self, entity):
-        data = self._format_data(entity)
+    def _create(self, pkg_dict, entity):
+        data = self._format_data(pkg_dict)
         extras = Extras(package=entity, owner=self.owner)
         extras.id = munge_name(' '.join(data['title'].split()))
 
         headers = self._default_headers()
         url = self.api_create.format(owner=self.owner)
         res = requests.post(url, data=json.dumps(data), headers=headers)
+
         if res.status_code < 300:
             model.Session.add(extras)
         else:
             log.error(res.content)
         return data
 
-    def _update(self, entity):
-        data = self._format_data(entity)
+    def _update(self, pkg_dict, entity):
+        data = self._format_data(pkg_dict)
         extras = entity.datadotworld_extras
 
         headers = self._default_headers()
@@ -118,20 +121,20 @@ class API:
             log.error(res.content)
         return data
 
-    def _format_data(self, entity):
+    def _format_data(self, pkg_dict):
         tags = []
-        tag_string = getattr(entity, 'tag_string', '')
-        if tag_string:
-            tags = tag_string.split(',')
+        notes = pkg_dict.get('notes')
+        for tag in pkg_dict.get('tags', []):
+            tags.append(tag['name'])
         data = dict(
-            title=entity.title,
-            description=truncate(entity.notes, 120),
-            summary=entity.notes,
+            title=pkg_dict['title'],
+            description=truncate(notes, 120),
+            summary=notes,
             tags=tags,
-            license=licenses.get(entity.license_id, 'Other'),
-            visibility='PRIVATE' if entity.private else 'OPEN'
+            license=licenses.get(pkg_dict.get('license_id'), 'Other'),
+            visibility='PRIVATE' if pkg_dict.get('private') else 'OPEN'
         )
-        files = self._prepare_resources(entity.resources)
+        files = _prepare_resources(pkg_dict.get('resources', []))
         if files:
             data['files'] = files
         return data
--- ckanext/datadotworld/tests/test_api.py
+++ ckanext/datadotworld/tests/test_api.py
@@ -1,23 +1,93 @@
 """Tests for plugin.py."""
+import ckan.model as model
+from ckan.model.types import make_uuid
 from ckan.model.domain_object import DomainObjectOperation as DOO
 import ckanext.datadotworld.plugin as plugin
-from ckan.tests.factories import Dataset, Organization
+from ckan.tests.helpers import call_action
+from ckan.tests.factories import Dataset, Organization, User
+from ckanext.datadotworld.model.credentials import Credentials
+from ckanext.datadotworld.model.extras import Extras
 from ckan.tests.helpers import (
     call_action, reset_db
 )
 from ckanext.datadotworld.model import create_tables
+from json import loads
 
 import mock
 from unittest import TestCase
+
 
 def setup_module():
     reset_db()
     create_tables()
 
+
 class TestAPI(TestCase):
-    #mock.patch('ckanext.datadotworld.api.create')
-    def test_provided_data(self, create=None):
-        org = Organization()
-        Dataset(
-            owner_org=org['id'],
-            tags=[dict(name='aa'), dict(name='bb')])
+    def setUp(self):
+        user = User()
+        org = Organization(users=[
+            {'capacity': 'admin', 'name': user['name']}
+        ])
+        creds = Credentials(
+            organization_id=org['id'],
+            integration=True,
+            key='key',
+            owner='owner'
+        )
+        model.Session.add(creds)
+        self.org = org
+        self.creds = creds
+        self.user = user
+
+    def _get_data(self, shallow=False, **kwargs):
+        data = dict(
+            owner_org=self.org['id']
+        )
+        if not shallow:
+            tags = [{'name': 'xxx'}]
+            data['tags'] = tags
+            data['title'] = 'Another  dataset'
+            data['notes'] = 'n' * 130
+            data['private'] = True
+            data['license_id'] = 'cc-by'
+        data.update(kwargs)
+        return data
+
+    def _get_extras(self):
+        q = model.Session.query(Extras)
+        return q.all()
+
+    @mock.patch('requests.post')
+    def test_create_data(self, post):
+        data = self._get_data(True, name='test-dataset')
+
+        call_action('package_create', {
+            'user': self.user['name']
+        }, **data)
+        self.assertEqual(1, post.call_count)
+        self.assertEqual(
+            data['name'], loads(post.call_args[1]['data'])['title'])
+        extr = self._get_extras()[-1]
+        self.assertEqual(extr.id, data['name'])
+
+        post.reset_mock()
+
+        data = self._get_data(name='another-dataset')
+        call_action('package_create', {
+            'user': self.user['name']
+        }, **data)
+        args = loads(post.call_args[1]['data'])
+        self.assertListEqual(
+            args['tags'], map(lambda t: t['name'], data['tags']))
+        self.assertEqual('PRIVATE', args['visibility'])
+        self.assertEqual(data['notes'], args['summary'])
+        self.assertEqual(data['notes'][:117] + '...', args['description'])
+        self.assertEqual('CC-BY', args['license'])
+        self.assertEqual(data['title'], args['title'])
+        extr = self._get_extras()[-1]
+        self.assertEqual(extr.id, data['title'].lower().replace('  ', '-'))
+
+    @mock.patch('requests.put')
+    @mock.patch('requests.post')
+    def test_update_data(self, post, put):
+        pkg = Dataset(user=self.user)
