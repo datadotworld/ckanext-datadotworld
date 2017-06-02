1643207842dad2308fd3ecef91b9fec447ca486a
Iaroslav Pereiaslovets <iaroslav1309@gmail.com>
2017-06-02 17:32:01 +1000

    updated license, added license info into all files

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -1,5 +1,19 @@
 #!/bin/sh -e
 
+# Copyright 2017 data.world, inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 echo "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
 sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
 sudo service jetty restart
