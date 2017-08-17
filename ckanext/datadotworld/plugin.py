6fbe60112299a575a7e543dbf46cd8160dc06604
Iaroslav Pereiaslovets <iaroslav1309@gmail.com>
2017-08-18 00:17:18 +1000

    0.3.0 (#22)
    * DWRLDXCEEZ-28 / recreate removed packages
    
    * refactoring
    
    * DWRLDXCEEZ-27 / complete test coverage of api
    
    * DWRLDXCEEZ-27 / updated readme
    
    * various wording changes
    
    * Fixed data.world labeling
    
    * changed data.world labeling
    
    * DWRLDXCEEZ-33 / changed mapping
    
    * DWRLDXCEEZ-34 / sync resource description
    
    * DWRLDXCEEZ-35 / resource extensions
    
    * WRLDXCEEZ-37 / description removed if empty
    
    * WRLDXCEEZ-37 / test update
    
    * DWRLDXCEEZ-38 / truncated resource description
    
    * DWRLDXCEEZ-36 / display deleted datasets
    
    * DWRLDXCEEZ-38 / do not break worlds
    
    * updated license, added license info into all files
    
    * updated license, added license info into all files
    
    * license requirements.txt fix
    
    * main license file fix:
    
    * DWRLDXCEEZ-39 / production installation
    
    * DWRLDXCEEZ-39 / README minor fix
    
    * DWRLDXCEEZ-42 / Add user-agent header to all requests
    
    * Updated extension version
    
    * DWRLDXCEEZ-42 / Add version number to user-agent header
    
    * DWRLDXCEEZ-43 / Delete datasets as they are deleted on CKAN
    
    * DWRLDXCEEZ-45 / Use PUT to create datasets
    
    * DWRLDXCEEZ-45 / Fix#1
    
    * DWRLDXCEEZ-46 / Normalize dataset tags
    
    * updated extension version
    
    * DWRLDXCEEZ-46 / Fix#1. Normalize tags before push
    
    * DWRLDXCEEZ-46 / Fix#2

--- ckanext/datadotworld/plugin.py
+++ ckanext/datadotworld/plugin.py
@@ -24,7 +24,9 @@ from ckan.lib.celery_app import celery
 import os
 from pylons import config
 
+
 log = logging.getLogger(__name__)
+
 
 def compat_enqueue(name, fn, args=None):
     u'''
@@ -38,6 +40,7 @@ def compat_enqueue(name, fn, args=None):
         # Fallback to Celery
         from ckan.lib.celery_app import celery
         celery.send_task(name, args=args)
+
 
 class DatadotworldPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
@@ -88,6 +91,14 @@ class DatadotworldPlugin(plugins.SingletonPlugin):
         return data_dict
 
     def after_update(self, context, data_dict):
+        ckan_ini_filepath = os.path.abspath(config['__file__'])
+        compat_enqueue(
+            'datadotworld.syncronize',
+            tasks.syncronize,
+            args=[data_dict['id'], ckan_ini_filepath])
+        return data_dict
+
+    def after_delete(self, context, data_dict):
         ckan_ini_filepath = os.path.abspath(config['__file__'])
         compat_enqueue(
             'datadotworld.syncronize',
