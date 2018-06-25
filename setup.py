b722bc77003cb379ee20dd9889de6c8ddbc1502c
Luke Tully <luke@luketully.ca>
2018-06-25 17:56:29 -0500

    Incremented sqlalchemy-migrate requirement (#35)
    * Removed dev-requirements installation step
    
    * Incremented sqlalchemy-migrate requirement

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ setup(
     # requirements files see:
     # https://packaging.python.org/en/latest/technical.html#install-requires-vs-requirements-files
     install_requires=[
-        'sqlalchemy-migrate==0.9.1',
+        'sqlalchemy-migrate==0.10.0',
         'celery==3.1.23'
     ],
 
